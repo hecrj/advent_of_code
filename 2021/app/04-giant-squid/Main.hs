de4dac80485e2e7fd517e46abe6064c6b17bb183
Héctor Ramón Jiménez <hector0193@gmail.com>
2021-12-04 16:32:06 +0700

    Solve day 4 (part 1) of AoC 2021 :tada:

---
+++ 2021/app/04-giant-squid/Main.hs
@@ -0,0 +1,102 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+
+main :: IO ()
+main = do
+    (inputNumbers : inputBoards) <- fmap (filter (not . null) . splitOn "\n\n") getContents
+
+    let
+        numbers =
+            map read (splitOn "," inputNumbers) :: [Int]
+
+        boards =
+            map parseBoard inputBoards
+
+    case bingo boards numbers of
+        Just bingo -> do
+            putStrLn $ show bingo
+            putStrLn $ show (score bingo)
+
+        Nothing ->
+            error "no bingo!"
+
+
+data Board
+    = Board [[Cell]]
+    deriving Show
+
+
+data Cell
+    = Unmarked Int
+    | Marked Int
+    deriving Show
+
+
+isMarked :: Cell -> Bool
+isMarked (Unmarked _) =
+    False
+isMarked (Marked _) =
+    True
+
+
+parseBoard :: String -> Board
+parseBoard =
+    Board . map (map (Unmarked . read) . filter (not . null) . splitOn " ") . lines
+
+
+data Bingo
+    = Bingo
+        { winner :: Board
+        , lastNumber :: Int
+        }
+    deriving Show
+
+
+bingo :: [Board] -> [Int] -> Maybe Bingo
+bingo _ [] =
+    Nothing
+bingo boards (number : rest) =
+    let
+        newBoards =
+            map (mark number) boards
+    in
+    case find isWinner newBoards of
+        Just winner ->
+            Just (Bingo winner number)
+
+        Nothing ->
+            bingo newBoards rest
+
+
+mark :: Int -> Board -> Board
+mark number (Board cells) =
+    Board (map (map replace) cells)
+    where
+        replace (Unmarked n)
+            | n == number =
+                Marked n
+            | otherwise =
+                Unmarked n
+        replace (Marked n) =
+            Marked n
+
+
+isWinner :: Board -> Bool
+isWinner (Board cells) =
+    any (all isMarked) cells || any (all isMarked) (transpose cells)
+
+
+score :: Bingo -> Int
+score (Bingo (Board cells) lastNumber) =
+    sum unmarkedNumbers * lastNumber
+    where
+        unmarkedNumbers =
+            map number (filter (not . isMarked) (concat cells))
+
+        number (Unmarked n) =
+            n
+        number (Marked n) =
+            n
