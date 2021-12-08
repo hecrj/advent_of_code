402ee606dd946e1db21aea5ec0e4e0e08148a756
Héctor Ramón Jiménez <hector0193@gmail.com>
2021-12-08 16:43:21 +0700

    Solve day 8 (part 2) of AoC 2021 :tada:
    ... by brute-force! :muscle:

--- 2021/app/08-seven-segment-search/Main.hs
+++ 2021/app/08-seven-segment-search/Main.hs
@@ -1,12 +1,17 @@
 module Main where
 
+import Data.Map.Strict (Map)
+import Data.Maybe
+import qualified Data.List as List
 import qualified Data.List.Split as List
+import qualified Data.Map.Strict as Map
 
 
 main :: IO ()
 main = do
     entries <- fmap (fmap parseEntry . lines) getContents
     putStrLn $ show $ sum (fmap uniqueOutputDigits entries)
+    putStrLn $ show $ sum (fmap translateOutput entries)
 
 
 data Entry
@@ -26,9 +31,48 @@ parseEntry input =
             error "invalid entry!"
 
 
+translateOutput :: Entry -> Int
+translateOutput (Entry patterns output) =
+    let
+        translation =
+            fromMaybe (error "translation not found!") $
+                List.find translatesAll allTranslations
+
+        digits =
+            catMaybes $ fmap (translate translation) output
+    in
+    sum (fmap (\( i, d ) -> 10 ^ i * toInt d) (zip [0..] (reverse digits)))
+    where
+        translatesAll translation =
+            all isJust (fmap (translate translation) patterns)
+
+
 uniqueOutputDigits :: Entry -> Int
 uniqueOutputDigits (Entry _ output) =
     length (filter isUnique output)
+
+
+data Translation
+    = Translation (Map Signal Signal)
+    deriving Show
+
+
+allTranslations :: [Translation]
+allTranslations =
+    fmap translationFromList (List.permutations (enumFrom A))
+
+
+translationFromList :: [Signal] -> Translation
+translationFromList =
+    Translation . Map.fromList . zip (enumFrom A)
+
+
+translate :: Translation -> Pattern -> Maybe Digit
+translate (Translation map) (Pattern signals) =
+    digitFromPattern (Pattern (fmap replace signals))
+    where
+        replace signal =
+            fromMaybe (error "invalid translation!") (Map.lookup signal map)
 
 
 data Signal
@@ -39,7 +83,7 @@ data Signal
     | E
     | F
     | G
-    deriving (Show, Eq, Ord)
+    deriving (Show, Eq, Ord, Enum)
 
 
 parseSignal :: Char -> Signal
@@ -63,7 +107,7 @@ parseSignal _ =
 
 data Pattern
     = Pattern [Signal]
-    deriving Show
+    deriving (Show, Eq)
 
 
 parsePattern :: String -> Pattern
@@ -74,6 +118,21 @@ parsePattern =
 isUnique :: Pattern -> Bool
 isUnique (Pattern signals) =
     length signals `elem` [ 2, 3, 4, 7 ]
+
+
+patternLength :: Pattern -> Int
+patternLength =
+    length . patternSignals
+
+
+patternSignals :: Pattern -> [Signal]
+patternSignals (Pattern signals) =
+    signals
+
+
+sort :: Pattern -> Pattern
+sort =
+    Pattern . List.sort . patternSignals
 
 
 data Digit
@@ -87,6 +146,12 @@ data Digit
     | Seven
     | Eight
     | Nine
+    deriving Enum
+
+
+digitFromPattern :: Pattern -> Maybe Digit
+digitFromPattern p =
+    List.find (\d -> digitPattern d == sort p) (enumFrom Zero)
 
 
 digitPattern :: Digit -> Pattern
@@ -110,3 +175,31 @@ digitPattern Eight =
     Pattern [ A, B, C, D, E, F, G ]
 digitPattern Nine =
     Pattern [ A, B, C, D, F, G ]
+
+
+toInt :: Digit -> Int
+toInt Zero =
+    0
+toInt One =
+    1
+toInt Two =
+    2
+toInt Three =
+    3
+toInt Four =
+    4
+toInt Five =
+    5
+toInt Six =
+    6
+toInt Seven =
+    7
+toInt Eight =
+    8
+toInt Nine =
+    9
+
+
+digitsWithLength :: Int -> [Digit]
+digitsWithLength n =
+    filter (\d -> patternLength (digitPattern d) == n) (enumFrom Zero)
