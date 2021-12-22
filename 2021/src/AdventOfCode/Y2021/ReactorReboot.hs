6a04e588fefda92f77189c8604e0427e490eb53e
Héctor Ramón Jiménez <hector0193@gmail.com>
2021-12-22 23:13:53 +0700

    Solve day 22 (part 2) of AoC 2021 :tada:

--- 2021/src/AdventOfCode/Y2021/ReactorReboot.hs
+++ 2021/src/AdventOfCode/Y2021/ReactorReboot.hs
@@ -5,6 +5,7 @@ module AdventOfCode.Y2021.ReactorReboot
 import Data.Set (Set)
 import qualified AdventOfCode
 import qualified Data.List.Split as List
+import qualified Data.Maybe as Maybe
 import qualified Data.Set as Set
 
 
@@ -16,7 +17,7 @@ day =
             count . initialize . parse
 
         part2 =
-            const 0
+            count . run . parse
 
 
 parse :: String -> [Step]
@@ -25,29 +26,35 @@ parse =
 
 
 data Reactor
-    = Reactor (Set Cube)
+    = Reactor (Set Region)
+    deriving Show
 
 
 initialize :: [Step] -> Reactor
 initialize =
-    foldr apply (Reactor Set.empty) . filter isInitialization . drop 2 . reverse
+    run . filter isInitialization . reverse . drop 2 . reverse
+
+
+run :: [Step] -> Reactor
+run =
+    foldr apply (Reactor Set.empty) . reverse
 
 
 apply :: Step -> Reactor -> Reactor
 apply (On region) (Reactor enabled) =
-    Reactor (Set.union enabled (cubes region))
+    case AdventOfCode.head (collisions region (Set.toList enabled)) of
+        Just collision ->
+            foldr apply (Reactor enabled) $ fmap On (split collision region)
+
+        Nothing ->
+            Reactor (Set.insert region enabled)
 apply (Off region) (Reactor enabled) =
-    Reactor (Set.difference enabled (cubes region))
+    Reactor $ Set.fromList $ concat $ remove region <$> Set.toList enabled
 
 
 count :: Reactor -> Int
-count (Reactor cubes) =
-    Set.size cubes
-
-
-data Cube
-    = Cube Position
-    deriving (Eq, Ord)
+count (Reactor regions) =
+    sum $ cubes <$> Set.toList regions
 
 
 data Step
@@ -69,6 +76,11 @@ parseStep input =
             error "invalid step!"
 
 
+isInitialization :: Step -> Bool
+isInitialization =
+    all (flip (<=) 50) . fmap abs . coordinates . region
+
+
 region :: Step -> Region
 region (On region) =
     region
@@ -76,17 +88,12 @@ region (Off region) =
     region
 
 
-isInitialization :: Step -> Bool
-isInitialization =
-    all (flip (<=) 50) . fmap abs . coordinates . region
-
-
 data Region
     = Region
         { start :: Position
         , end :: Position
         }
-    deriving Show
+    deriving (Eq, Ord, Show)
 
 
 parseRegion :: String -> Region
@@ -103,7 +110,7 @@ parseRegion input =
                 ( zStart, zEnd ) =
                     parseRange z
             in
-            Region (Position xStart yStart zStart) (Position xEnd yEnd zEnd)
+            Region (Position xStart yStart zStart) (Position (xEnd + 1) (yEnd + 1) (zEnd + 1))
 
         _ ->
             error "invalid region!"
@@ -119,26 +126,86 @@ parseRegion input =
 
 coordinates :: Region -> [Int]
 coordinates (Region (Position xStart yStart zStart) (Position xEnd yEnd zEnd)) =
-    [ xStart, yStart, zStart, xEnd, yEnd, zEnd ]
+    [ xStart, yStart, zStart, xEnd - 1, yEnd - 1, zEnd - 1 ]
 
 
-cubes :: Region -> Set Cube
+cubes :: Region -> Int
 cubes (Region (Position xStart yStart zStart) (Position xEnd yEnd zEnd)) =
-    Set.fromList $
-        Cube
-            <$> [
-                Position x y z
-                | x <- range xStart xEnd
-                , y <- range yStart yEnd
-                , z <- range zStart zEnd
-            ]
+    let
+        x =
+            max 0 (xEnd - xStart)
+
+        y =
+            max 0 (yEnd - yStart)
+
+        z =
+            max 0 (zEnd - zStart)
+    in
+    x * y * z
 
 
-range :: Int -> Int -> [Int]
-range start end =
-    enumFromThenTo start (start + 1) end
+collisions :: Region -> [Region] -> [Region]
+collisions region =
+    Maybe.catMaybes . fmap (collision region)
+
+
+collision :: Region -> Region -> Maybe Region
+collision (Region startR endR) (Region startT endT) =
+    let
+        candidate =
+            Region (maximize startR startT) (minimize endR endT)
+    in
+    if cubes candidate > 0 then
+        Just candidate
+
+    else
+        Nothing
+
+
+remove :: Region -> Region -> [Region]
+remove region target =
+    case collision region target of
+        Just collision ->
+            split collision target
+
+        Nothing ->
+            [ target ]
+
+
+split :: Region -> Region -> [Region]
+split (Region startR endR) (Region startT endT) =
+    filter ((/=) 0 . cubes)
+        [ Region
+            startT
+            (Position (x endT) (y endT) (z startR))
+        , Region
+            (Position (x startT) (y startT) (z startR))
+            (Position (x startR) (y endT) (z endR))
+        , Region
+            (Position (x endR) (y startT) (z startR))
+            (Position (x endT) (y endT) (z endR))
+        , Region
+            (Position (x startR) (y startT) (z startR))
+            (Position (x endR) (y startR) (z endR))
+        , Region
+            (Position (x startR) (y endR) (z startR))
+            (Position (x endR) (y endT) (z endR))
+        , Region
+            (Position (x startT) (y startT) (z endR))
+            endT
+        ]
 
 
 data Position
     = Position { x :: Int, y :: Int, z :: Int }
     deriving (Eq, Ord, Show)
+
+
+maximize :: Position -> Position -> Position
+maximize (Position x1 y1 z1) (Position x2 y2 z2) =
+    Position (max x1 x2) (max y1 y2) (max z1 z2)
+
+
+minimize :: Position -> Position -> Position
+minimize (Position x1 y1 z1) (Position x2 y2 z2) =
+    Position (min x1 x2) (min y1 y2) (min z1 z2)
