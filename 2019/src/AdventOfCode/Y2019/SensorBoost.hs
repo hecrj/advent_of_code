aec76b03185de077d76279cba5ece4cca780816d
Héctor Ramón Jiménez <hector0193@gmail.com>
2021-12-31 18:10:28 +0700

    Solve day 9 (part 2) of AoC 2019

--- 2019/src/AdventOfCode/Y2019/SensorBoost.hs
+++ 2019/src/AdventOfCode/Y2019/SensorBoost.hs
@@ -11,7 +11,10 @@ day =
     AdventOfCode.day "Sensor Boost" part1 part2
     where
         part1 =
-            head . snd . Intcode.run (Intcode.input [ 1 ]) . Intcode.load . Intcode.parse
+            run 1
 
         part2 =
-            const 0
+            run 2
+
+        run n =
+            head . snd . Intcode.run (Intcode.input [ n ]) . Intcode.load . Intcode.parse
