73e30dac237f1a221d301f830d531c4192b611a9
Héctor Ramón Jiménez <hector@hecrj.dev>
2023-12-03 23:07:57 +0100

    Solve day 3 (part 2) of AoC 2023 :tada:

--- 2023/src/AdventOfCode/Y2023/GearRatios.hs
+++ 2023/src/AdventOfCode/Y2023/GearRatios.hs
@@ -1,6 +1,7 @@
 module AdventOfCode.Y2023.GearRatios (day) where
 
 import qualified AdventOfCode
+import qualified Data.Bifunctor
 import qualified Data.Char as Char
 import qualified Data.List as List
 
@@ -14,12 +15,12 @@ day = AdventOfCode.day "Gear Ratios" part1 part2
 
 part1 :: AdventOfCode.Solution
 part1 =
-    sum . partNumbers . parse
+    sum . fmap value . partNumbers . parse
 
 
 part2 :: AdventOfCode.Solution
 part2 =
-    const 0
+    sum . fmap gearRatio . gears . parse
 
 
 newtype Schematic = Schematic [[Char]]
@@ -36,7 +37,7 @@ data Cell = Cell
     { _i :: Int
     , _j :: Int
     }
-    deriving (Show)
+    deriving (Show, Eq)
 
 
 parse :: String -> Schematic
@@ -44,9 +45,9 @@ parse =
     Schematic . lines
 
 
-partNumbers :: Schematic -> [Int]
+partNumbers :: Schematic -> [Number]
 partNumbers schematic =
-    fmap value $ filter (isPartNumber schematic) $ numbers schematic
+    filter (isPartNumber schematic) $ numbers schematic
 
 
 numbers :: Schematic -> [Number]
@@ -98,6 +99,41 @@ neighbors (Cell i j) =
             ]
 
 
+newtype Gear = Gear (Number, Number)
+
+
+gears :: Schematic -> [Gear]
+gears schematic@(Schematic rows) =
+    let
+        numbers = partNumbers schematic
+        candidates = fst <$> filter (\(_cell, value) -> value == '*') (cells rows)
+    in
+        fmap toGear $
+            filter ((==) 2 . List.length) $
+                fmap (adjacent numbers) candidates
+  where
+    adjacent numbers candidate =
+        let
+            gearNeighbors = neighbors candidate
+        in
+            filter (\(Number _ cells) -> any (`List.elem` gearNeighbors) cells) numbers
+
+    toGear [n1, n2] = Gear (n1, n2)
+    toGear _ = error "invalid gear!"
+
+
+gearRatio :: Gear -> Int
+gearRatio (Gear (n1, n2)) =
+    value n1 * value n2
+
+
 enumerate :: [a] -> [(Int, a)]
 enumerate =
     zip [0 ..]
+
+
+cells :: [[a]] -> [(Cell, a)]
+cells =
+    concatMap (\(i, row) -> fmap (Data.Bifunctor.first (Cell i)) row)
+        . enumerate
+        . fmap enumerate
