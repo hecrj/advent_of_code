9c8947ab720538a1dd5fb089d1230d6bd5bf4d94
Héctor Ramón Jiménez <hector@hecrj.dev>
2023-12-03 19:56:20 +0100

    Solve day 1 (part 2) of AoC 2023 :tada:

--- 2023/src/AdventOfCode/Y2023/Trebuchet.hs
+++ 2023/src/AdventOfCode/Y2023/Trebuchet.hs
@@ -2,6 +2,10 @@ module AdventOfCode.Y2023.Trebuchet (day) where
 
 import qualified AdventOfCode
 import qualified Data.Char as Char
+import qualified Data.List as List
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.Internal.Search as Search
 
 
 day :: AdventOfCode.Day
@@ -16,7 +20,7 @@ part1 =
 
 part2 :: String -> Int
 part2 =
-    const 0
+    sum . fmap (accurateCalibration . Text.pack) . lines
 
 
 calibration :: String -> Int
@@ -27,3 +31,33 @@ calibration line =
         lastDigit = Char.digitToInt $ head (reverse digits)
     in
         firstDigit * 10 + lastDigit
+
+
+accurateCalibration :: Text -> Int
+accurateCalibration line =
+    let
+        foundDigits = findDigits digits line
+    in
+        head foundDigits * 10 + last foundDigits
+  where
+    digits =
+        [ ("one", 1)
+        , ("two", 2)
+        , ("three", 3)
+        , ("four", 4)
+        , ("five", 5)
+        , ("six", 6)
+        , ("seven", 7)
+        , ("eight", 8)
+        , ("nine", 9)
+        ]
+
+    findDigits digits line =
+        fmap snd $ List.sortOn fst $ concatMap (uncurry digitIndices) digits
+      where
+        digitIndices target value =
+            let
+                targetIndices = Search.indices target line
+                characterIndices = Search.indices (Text.singleton (Char.intToDigit value)) line
+            in
+                fmap (,value) (targetIndices ++ characterIndices)
